d147f1677e8102872cdbee89b0ee6f96b3b7f522
Saravanakumar <saravanais2020@gmail.com>
2026-03-24 14:37:12 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
       return;
     }
 
-    axios.post("https://bulk-mail-app-1-66zs.onrender.com/", {
+    axios.post("https://bulk-mail-app-1-66zs.onrender.com/sendmail", {
       subject,
       msg,
       emails: emails.split(",")
